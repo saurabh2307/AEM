42072dd0f94fbd6ffe3caa4a29e592e7192a937d
saurabh2307 <saurabh.khatri@outlook.com>
2017-08-02 10:44:05 +0530

    Change-Id: I867dd8aaf08d3fd2fdf5d66cc866f7f143f3b079 Signed-off-by: saurabh2307 <saurabh.khatri@outlook.com>

--- src/com/git/HelloWorld.java
+++ src/com/git/HelloWorld.java
@@ -2,10 +2,12 @@ package com.git;
 
 public class HelloWorld {
 
+	//This is main method
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World");
+		System.out.println("well done");
 
 	}
 
